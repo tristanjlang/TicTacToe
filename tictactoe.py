cae921a7f09627ff2c53229c6f3222eba36dd314
tristanjlang <tristan@alum.mit.edu>
2013-11-25 20:58:12 -0500

    initial pseudocode added

---
+++ tictactoe.py
@@ -0,0 +1,21 @@
+##############
+# PSEUDOCODE #
+##############
+
+# [  A B C]
+# [1      ]
+# [2      ]
+# [3      ]
+
+# 1. set board equal to the following, and print:
+# [[  A B C], [       ], [1      ], [2      ], [3      ]]
+
+# 2. prompt user1 for input regarding the row he wants to enter an X/O into
+#       >> http://stackoverflow.com/questions/70797/python-and-user-input
+# 3. prompt user1 for input regarding the col he wants to enter an X/O into
+# 4. update board and print
+# 5. check diagonal, row, and column for three consecutive X/O's to declare win
+# 6. check for full board to declare tie
+# 7. repeat steps 2-6 for input from user2
+# 8. repeat steps 2-7 until board is full or winner is declared
+
